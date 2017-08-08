715652b9a5c40404b45fec1b09be2a9fdea597c8
Luna <arthu.lee.ac72@outlook.com>
2017-08-08 14:58:40 +0800

    gitignore

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import './main.css'
+import './section-left.css'
+import './section-right.css'
+import './section-left'
+import './section-right'
 
 // console.log('hello, world');
 $(document).ready(() => {
     console.log('hello');
-})
+});
