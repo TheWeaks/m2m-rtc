a35da2b00a8b00119056932b8cc88ee1dfc1222c
Luna <arthu.lee.ac72@outlook.com>
2017-08-11 10:14:18 +0800

    test update

--- src/section.js
+++ src/section.js
@@ -4,13 +4,19 @@ import $ from 'jquery'
 let p = navigator.mediaDevices.getUserMedia({audio: true, video: {width: 1280, height: 720}});
 
 p.then(function (mediaStream) {
-	let video = document.querySelector('#main-stream');
-	video.src = window.URL.createObjectURL(mediaStream);
-	video.onloadedmetadata = function (e) {
-		// Do something with the video here.
-	};
+    let video = document.querySelector('#main-stream');
+    let subvideo1 = document.querySelector('#substream-1');
+    let subvideo2 = document.querySelector('#substream-2');
+    let subvideo3 = document.querySelector('#substream-3');
+    video.src = window.URL.createObjectURL(mediaStream);
+    subvideo1.src = window.URL.createObjectURL(mediaStream);
+    subvideo2.src = window.URL.createObjectURL(mediaStream);
+    subvideo3.src = window.URL.createObjectURL(mediaStream);
+    video.onloadedmetadata = function (e) {
+        // Do something with the video here.
+    };
 });
 
 p.catch(function (err) {
-	console.log(err.name);
+    console.log(err.name);
 });
